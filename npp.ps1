ba217ac7144d6939417e40cfc6e0857a0a654e5a
KnapSac <casper.verhaar@gmail.com>
2020-01-10 12:00:29 +0100

    Make params optional for 'npp' and 'vs' aliases

--- npp.ps1
+++ npp.ps1
@@ -1,3 +1,10 @@
 param([String]$file="")
 
-Start-Process -FilePath $npp_path -ArgumentList $file
+if ($file)
+{
+    Start-Process -FilePath $npp_path -ArgumentList $file
+}
+else
+{
+    Start-Process -FilePath $npp_path
+}
--- vs.ps1
+++ vs.ps1
@@ -1,3 +1,10 @@
 param([String]$sln="")
 
-Start-Process -FilePath $vs_path -ArgumentList $sln
+if ($sln)
+{
+    Start-Process -FilePath $vs_path -ArgumentList $sln
+}
+else
+{
+    Start-Process -FilePath $vs_path
+}
