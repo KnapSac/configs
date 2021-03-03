d72fe7bda62bc6ce9bbb5715afd8e363f63f63c4
Casper Verhaar <casper.verhaar@gmail.com>
2021-03-03 10:07:43 +0100

    Remove unused functions

--- PsScripts/PsScripts.psm1
+++ PsScripts/PsScripts.psm1
@@ -6,34 +6,20 @@ function Set-PoshGitSettings {
     $GitPromptSettings.DefaultPromptAbbreviateGitDirectory = $True
 }
 
-function Use-ShortGit {
-    git $args
-}
-
 function Show-Log {
-    if (Test-Path ".\.git" -PathType Container) {
-        git log --all --graph --decorate --oneline
-    }
-    elseif (Test-Path ".\.svn" -PathType Container) {
+    if (Test-Path ".\.svn" -PathType Container) {
         TortoiseProc.exe /command:log /path:.
+    } else {
+        Write-Output "This directory is not a SVN repository"
     }
-    else {
-        Write-Output "This directory is not a Git or SVN repository"
-    }
-}
-
-function Update-Config {
-    git --git-dir 'I:\Configs\' --work-tree ((Get-Location).Path + '\') $args
 }
 
 function Show-Modifications {
     if (Test-Path ".\.git" -PathType Container) {
         TortoiseGitProc.exe /command:repostatus /path:.
-    }
-    elseif (Test-Path ".\.svn" -PathType Container) {
+    } elseif (Test-Path ".\.svn" -PathType Container) {
         TortoiseProc.exe /command:repostatus /path:.
-    }
-    else {
+    } else {
         Write-Output "This directory is not a Git or SVN repository"
     }
 }
@@ -58,39 +44,15 @@ function Start-VisualStudio {
     }
 }
 
-function Start-NeoVim {
-    nvim-qt $args
-}
-
 function Update-NeoVim {
     nvim +PlugInstall +qa
 }
 
-function New-File { 
+function New-File {
     param (
         [parameter (Mandatory = $true)]
         [String]$FileName
     )
-    
+
     Out-File -force -FilePath $FileName
-}
-  
-
-#TODO CV: Refactor profiles to be project specific
-function Open-WorkEnv {
-    Set-Location $work_dir
-    vs $work_sln_path
-    Start-Process $browser$work_url
-    Start-Process -FilePath $console_path -Verb RunAs
-    Start-Process -FilePath $baretail_path
-    mods
-    log
-}
-
-function Open-UniEnv {
-    Set-Location $uni_dir
-    Start-VisualStudio $uni_sln_path
-    #code .
-    Start-Process $browser$github_url
-    mods
 }
