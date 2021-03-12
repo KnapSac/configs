cc84556828807bae834d8d97450dccbe88f63591
Casper Verhaar <casper.verhaar@gmail.com>
2021-03-12 11:51:01 +0100

    Add powershell aliases

---
+++ shell/pwsh/aliases.psm1
@@ -0,0 +1,8 @@
+New-Alias -Name galias -Value git-alias-prettifier
+New-Alias -Name mods -Value Show-Modifications
+New-Alias -Name n -Value nvim.exe
+New-Alias -Name touch -Value New-File
+New-Alias -Name upvim -Value Update-NeoVim
+New-Alias -Name vs -Value Start-VisualStudio
+
+Export-ModuleMember -Alias *
