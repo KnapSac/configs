b8ff0f524c83d8431e53ffdcc6bd39a2a6c7fd27
Casper Verhaar <casper.verhaar@gmail.com>
2022-11-16 21:06:24 +0100

    nvim: always select completion

--- editor/nvim/after/plugin/lsp.lua
+++ editor/nvim/after/plugin/lsp.lua
@@ -8,46 +8,46 @@ local sumneko_binary = sumneko_root_path .. "/bin/lua-language-server"
 -- Setup nvim-cmp.
 local cmp = require("cmp")
 cmp.setup({
-	mapping = cmp.mapping.preset.insert({
-		["<C-u>"] = cmp.mapping.scroll_docs(-4),
-		["<C-d>"] = cmp.mapping.scroll_docs(4),
+    mapping = cmp.mapping.preset.insert({
+        ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-d>"] = cmp.mapping.scroll_docs(4),
         ["<CR>"] = cmp.mapping.confirm({ select = true }),
         ["<Tab>"] = function(fallback)
             if cmp.visible() then
-                cmp.select_next_item()
+                cmp.select_next_item({ select = true })
             else
                 fallback()
             end
         end,
         ["<S-Tab>"] = function(fallback)
             if cmp.visible() then
-                cmp.select_prev_item()
+                cmp.select_prev_item({ select = true })
             else
                 fallback()
             end
         end,
-	}),
+    }),
 
-	sources = {
-		{ name = "nvim_lsp" },
-		{ name = "buffer" },
-	},
+    sources = {
+        { name = "nvim_lsp" },
+        { name = "buffer" },
+    },
 })
 
 local function config(_config)
-	return vim.tbl_deep_extend("force", {
-		on_attach = function()
-			nnoremap("gd", function() vim.lsp.buf.definition() end)
-			nnoremap("K", function() vim.lsp.buf.hover() end)
-			nnoremap("<leader>ws", function() vim.lsp.buf.workspace_symbol() end)
-			nnoremap("<leader>d", function() vim.diagnostic.open_float() end)
-			nnoremap("[d", function() vim.diagnostic.goto_next() end)
-			nnoremap("]d", function() vim.diagnostic.goto_prev() end)
-			nnoremap("<leader>a", function() vim.lsp.buf.code_action() end)
-			nnoremap("<leader>gr", function() vim.lsp.buf.references() end)
-			inoremap("<C-h>", function() vim.lsp.buf.signature_help() end)
-		end,
-	}, _config or {})
+    return vim.tbl_deep_extend("force", {
+        on_attach = function()
+            nnoremap("gd", function() vim.lsp.buf.definition() end)
+            nnoremap("K", function() vim.lsp.buf.hover() end)
+            nnoremap("<leader>ws", function() vim.lsp.buf.workspace_symbol() end)
+            nnoremap("<leader>d", function() vim.diagnostic.open_float() end)
+            nnoremap("[d", function() vim.diagnostic.goto_next() end)
+            nnoremap("]d", function() vim.diagnostic.goto_prev() end)
+            nnoremap("<leader>a", function() vim.lsp.buf.code_action() end)
+            nnoremap("<leader>gr", function() vim.lsp.buf.references() end)
+            inoremap("<C-h>", function() vim.lsp.buf.signature_help() end)
+        end,
+    }, _config or {})
 end
 
 -- Typescript
@@ -66,31 +66,31 @@ require("lspconfig").cssls.setup(config())
 
 -- Rust
 require("lspconfig").rust_analyzer.setup(config({
-	cmd = { "rustup", "run", "beta", "rust-analyzer" },
+    cmd = { "rustup", "run", "beta", "rust-analyzer" },
 }))
 
 -- Lua
 require("lspconfig").sumneko_lua.setup(config({
-	cmd = { sumneko_binary, "-E", sumneko_root_path .. "/main.lua" },
-	settings = {
-		Lua = {
-			runtime = {
-				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-				version = "LuaJIT",
-				-- Setup your lua path
-				path = vim.split(package.path, ";"),
-			},
-			diagnostics = {
-				-- Get the language server to recognize the `vim` global
-				globals = { "vim" },
-			},
-			workspace = {
-				-- Make the server aware of Neovim runtime files
-				library = {
-					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-					[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-				},
-			},
-		},
-	},
+    cmd = { sumneko_binary, "-E", sumneko_root_path .. "/main.lua" },
+    settings = {
+        Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = "LuaJIT",
+                -- Setup your lua path
+                path = vim.split(package.path, ";"),
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { "vim" },
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = {
+                    [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                    [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+                },
+            },
+        },
+    },
 }))
