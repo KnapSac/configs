6a0648f3eae232d380645c733d7e048767be2b5a
Casper Verhaar <casper.verhaar@gmail.com>
2023-02-28 20:47:25 +0100

    nvim: format on save

--- editor/nvim/lua/knapsac/set.lua
+++ editor/nvim/lua/knapsac/set.lua
@@ -34,6 +34,15 @@ vim.api.nvim_create_autocmd("BufEnter", {
         vim.opt.fileformat = "dos"
     end,
 })
+vim.api.nvim_create_autocmd("BufWritePre", {
+    callback = function()
+        if vim.lsp.buf.format then
+          vim.lsp.buf.format()
+        elseif vim.lsp.buf.formatting then
+          vim.lsp.buf.formatting()
+        end
+    end
+})
 
 -- Always show the signcolumn
 vim.opt.signcolumn = 'yes'
